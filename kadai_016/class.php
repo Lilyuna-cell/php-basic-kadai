cb1eebe49155f8d484eb8ad90babef7b68674d7f
Lilyuna-cell <papyui0505@icloud.com>
2025-08-25 19:45:37 +0000

    add files

---
+++ kadai_016/class.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>
+<?php
+    
+    
+class Food {
+      private $name;
+      private $price;
+  // コンストラクタを定義する
+      public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+      public function show_price() {
+          return $this->price;
+  }
+}
+class Animal{
+      private $name;
+      private $height;
+      private $weight;
+   // コンストラクタを定義する
+    public function __construct(string $name, int $height, string $weight) {
+      $this->name = $name;
+      $this->height = $height;
+      $this->weight = $weight;
+    }
+    public function show_height(){
+        return $this->height;
+    }
+    }
+    $food = new Food('potato', '250');
+    $animal = new Animal('dog', '60', '5000');
+
+    print_r($food);
+    echo"<br>";
+    print_r($animal);
+
+    echo "<br>";
+    echo $food->show_price();
+    echo"<br>";
+    echo $animal->show_height();
+
+    ?>
+
+  </p>
+</body>
+</html>  
